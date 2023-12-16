90407533c1e3b511b2370e3476b55bb629a7d53f
thooton <thooton@thooton.com>
2023-12-15 18:47:17 -0700

    qqq

--- gguf-py/gguf/tensor_mapping.py
+++ gguf-py/gguf/tensor_mapping.py
@@ -273,10 +273,7 @@ class TensorNameMap:
                     tensor_name = TENSOR_NAMES[tensor].format(bid = bid, xid = xid)
                     self.mapping[tensor_name] = (tensor, tensor_name)
                     for key in keys:
-                        try:
-                            key = key.format(bid = bid, xid = xid)
-                        except Exception:
-                            print(key)
+                        key = key.format(bid = bid, xid = xid)
                         self.mapping[key] = (tensor, tensor_name)
 
     def get_type_and_name(self, key: str, try_suffixes: Sequence[str] = ()) -> tuple[MODEL_TENSOR, str] | None:
