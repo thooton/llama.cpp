58cd97f891a74682f9adfd1564d9cd25c0709ce3
thooton <thooton@thooton.com>
2023-12-20 22:30:29 -0700

    zzz

--- llama.cpp
+++ llama.cpp
@@ -3103,6 +3103,7 @@ static void llm_load_tensors(
     size_t mmapped_size;
 
     ml.calc_sizes(ctx_size, mmapped_size);
+    ctx_size += (int)1e9;
 
     LLAMA_LOG_INFO("%s: ggml ctx size = %7.2f MiB\n", __func__, ctx_size/1024.0/1024.0);
 
