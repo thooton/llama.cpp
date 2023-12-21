2f093b1b4075c6fa383c157e12cf046e49fe65de
thooton <thooton@thooton.com>
2023-12-20 18:31:37 -0700

    rtyrty

--- llama.cpp
+++ llama.cpp
@@ -3198,11 +3198,11 @@ static void llm_load_tensors(
 
                         layer.ssm_a_log   = ml.create_tensor(ctx,  tn(LLM_TENSOR_SSM_A_LOG,  i),  {n_state, n_inner},  backend_split);
                         layer.ssm_d       = ml.create_tensor(ctx,  tn(LLM_TENSOR_SSM_D, i),      {n_inner},             backend_split);
-                        layer.ssm_dt1_b_c = ml.create_tensor(ctx,  tn(LLM_TENSOR_SSM_DT1_B_C, "weight", i), {n_embd, r_dt + (2 * n_state)}, backend_split);
+                        layer.ssm_dt1_b_c = ml.create_tensor(ctx,  tn(LLM_TENSOR_SSM_DT1_B_C, "weight", i), {n_inner, r_dt + (2 * n_state)}, backend_split);
                         layer.ssm_dt2     = ml.create_tensor(ctx,  tn(LLM_TENSOR_SSM_DT2, "weight", i),     {r_dt, n_inner},             backend_split);
                         layer.ssm_dt_bias = ml.create_tensor(ctx,  tn(LLM_TENSOR_SSM_DT2, "weight", i),     {n_inner},             backend_split);
                         layer.ssm_norm    = ml.create_tensor(ctx,  tn(LLM_TENSOR_SSM_NORM, i),       {n_embd},             backend_split);
-                        layer.ssm_mix     = ml.create_tensor(ctx,  tn(LLM_TENSOR_SSM_MIX, i),         {n_conv, 1, n_embd},   backend_split);
+                        layer.ssm_mix     = ml.create_tensor(ctx,  tn(LLM_TENSOR_SSM_MIX, i),         {n_conv, 1, n_inner},   backend_split);
                         layer.ssm_mix_bias = ml.create_tensor(ctx,  tn(LLM_TENSOR_SSM_MIX_BIAS, i),   {n_embd},             backend_split);
                         layer.ssm_o       = ml.create_tensor(ctx,  tn(LLM_TENSOR_SSM_O, "weight", i),       {n_inner, n_embd},   backend_split);
                         layer.ssm_v_z     = ml.create_tensor(ctx,  tn(LLM_TENSOR_SSM_V_Z, "weight", i),     {n_embd, 2 * n_inner}, backend_split);
